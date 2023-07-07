3832c767ca496d9cc838552fcee5a0fec459f3a2
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-07-07 11:21:07 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Try_and_Catch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format exception, please enter the correct type next time");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow exception , the number was too long or too short for an int32");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("ArgumentNull exception , the number was too long or too short for an int32");
+            }
+            finally
+            {
+                Console.WriteLine("");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
